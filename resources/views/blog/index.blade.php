7ec41401bb3c00fc0deb16157e37e48f7a92dd92
khoirony <khoironyarief08@gmail.com>
2022-12-12 16:09:24 +0700

    update blog

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -2,7 +2,7 @@
 
     @section("content")
     <br>
-    <a href="{{ route('produk.create') }}" class="btn btn-primary">Tambah Tulisan</a>
+    <a href="{{ route('blog.create') }}" class="btn btn-primary">Tambah Tulisan</a>
     
     <br>
 
@@ -13,32 +13,28 @@
                 <th scope="col" width="20%">Gambar</th>
                 <th scope="col">Judul</th>
                 <th scope="col">Konten</th>
-                <th scope="col">waktu</th>
-                <th scope="col">Stok</th>
+                <th scope="col">kategori</th>
                 <th scope="col">Aksi</th>
             </tr>
         </thead>
         <tbody>
-            @foreach ($produk as $p)
+            @foreach ($blog as $b)
             <tr>
                 <td width="20%">
-                    <img src="{{$p->gambar_produk}}" alt="gambar produk" class="img-thumbnail">
+                    <img src="{{$b->thumbnail}}" alt="gambar blog" class="img-thumbnail">
                 </td>
                 <td>
-                    {{$p->nama_produk}}
+                    {{$b->judul}}
                 </td>
                 <td>
-                    {{$p->deskripsi_produk}}
+                    {{$b->konten}}
                 </td>
                 <td>
-                    {{$p->harga_produk}}
-                </td>
-                <td>
-                    {{$p->stok_produk}}
+                    {{$b->id_kategori}}
                 </td>
                 <td width="17%">
-                    <a href="{{route('produk.detail',$p->id)}}" class="btn btn-primary me-2">Detail</a>
-                    <a href="{{route('produk.destroy',$p->id)}}" class="btn btn-danger">Delete</a>
+                    <a href="{{route('blog.detail',$b->id)}}" class="btn btn-primary me-2">Detail</a>
+                    <a href="{{route('blog.destroy',$b->id)}}" class="btn btn-danger">Delete</a>
                 </td>
             </tr>
             @endforeach
